6b4805f2b033fcd4c64cf3f7279a61a0f7dfc4fc
kocokolo <pengcui@live.com>
2015-05-14 20:08:20 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -122,3 +122,26 @@ var getTransparentPercent=function (ctx, width, height) {
         }
         return (transPixs.length / (pixles.length / 4) * 100).toFixed(2);
     },
+    // canvas get the deal position .
+   getRelativePosition = helpers.getRelativePosition = function(evt){
+			var mouseX, mouseY;
+			var e = evt.originalEvent || evt,
+				canvas = evt.currentTarget || evt.srcElement,
+				boundingRect = canvas.getBoundingClientRect();
+
+			if (e.touches){
+				mouseX = e.touches[0].clientX - boundingRect.left;
+				mouseY = e.touches[0].clientY - boundingRect.top;
+
+			}
+			else{
+				mouseX = e.clientX - boundingRect.left;
+				mouseY = e.clientY - boundingRect.top;
+			}
+
+			return {
+				x : mouseX,
+				y : mouseY
+			};
+
+		},

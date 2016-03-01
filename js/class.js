cb594a739aaa7ca72355f9db0b529692fa71d10b
kocokolo <pengcui@live.com>
2016-03-01 13:32:03 +0800

    Update class.js

--- js/class.js
+++ js/class.js
@@ -1,12 +1,44 @@
 // js 实现面向对象类的继承，构造函数可能动态添加的属性和方法以及父元型对象的属性和方法
-
-function inherite (subclass, superclass) {
+// [1]
+function inherite (Self, Super) {
 	"use strict";
 
-	function o() { this.constructor = subclass; }
-	o.prototype = superclass.prototype;
-	return (subclass.prototype = new o());
+	function o() { this.constructor = Self; }
+	o.prototype = Super.prototype;
+	
+	return (Self.prototype = new o());
 }
+// [2]
+function inherits(Self, Super) {
+    var Temp = function () {
+    };
+    var Func = function () {
+        Super.apply(this, arguments);
+        Self.apply(this, arguments);
+    };
+    Func._super = Super.prototype;
+    Temp.prototype = Super.prototype;
+    Func.prototype = new Temp();
+
+    return Func;
+}
+/**
+ * @private
+ * @deprecated
+ * Implementaton of inheritance for Javascript objects
+ * e.g. Class can access all of Base's function prototypes
+ * <pre lang="javascript"><code>
+ * Base = function () {}
+ * Class = function () {}
+ * Class = Class.extendsFrom(Base)
+ * </code></pre>
+ * @param {Object} Super
+ */
+Function.prototype.extendsFrom = function (Super) {
+    return inherits(this,Super);
+};
+
+
 
 // 方法提升 实现类型面向对象，多态概念，方法的重载
 function promote(subclass, prefix) {

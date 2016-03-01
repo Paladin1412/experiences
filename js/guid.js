b598d9b756ec8a2a818b945655ab0e00c02cc621
kocokolo <pengcui@live.com>
2016-03-01 13:25:56 +0800

    Update guid.js

--- js/guid.js
+++ js/guid.js
@@ -11,3 +11,21 @@ module.exports = (function () {
            s4() + '-' + s4() + s4() + s4();
   };
 })();
+
+module.exports =function() {
+    return {
+        /**
+         * @function
+         * @name pc.guid.create
+         * @description Create an RFC4122 version 4 compliant GUID
+         * @return {String} A new GUID
+         */
+        create: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, 
+                    v = (c == 'x') ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        }
+    };
+}()

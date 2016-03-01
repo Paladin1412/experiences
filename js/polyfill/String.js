7344897626e67849363612d1c6a684014e974cc3
kocokolo <pengcui@live.com>
2016-03-01 13:19:56 +0800

    Create String.js

---
+++ js/polyfill/String.js
@@ -0,0 +1,28 @@
+if (! String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function(str) {
+            var that = this;
+            for(var i = 0, ceil = str.length; i < ceil; i++)
+                if(that[i] !== str[i]) return false;
+            return true;
+        }
+    });
+}
+
+if (! String.prototype.endsWith) {
+    Object.defineProperty(String.prototype, 'endsWith', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function(str) {
+            var that = this;
+            for(var i = 0, ceil = str.length; i < ceil; i++)
+                if (that[i + that.length - ceil] !== str[i])
+                    return false;
+            return true;
+        }
+    });
+}

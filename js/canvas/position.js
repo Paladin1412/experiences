5cbb4578aa2acea01d85cfe14cf2564e4a8678f6
kocokolo <pengcui@live.com>
2016-03-02 15:05:55 +0800

    Create position.js

---
+++ js/canvas/position.js
@@ -0,0 +1,21 @@
+ function getRelativePosition(evt){
+			var mouseX, mouseY;
+			var e = evt.originalEvent || evt,
+				canvas = evt.currentTarget || evt.srcElement,
+				boundingRect = canvas.getBoundingClientRect();
+
+			if (e.touches){
+				mouseX = e.touches[0].clientX - boundingRect.left;
+				mouseY = e.touches[0].clientY - boundingRect.top;
+			}
+			else{
+				mouseX = e.clientX - boundingRect.left;
+				mouseY = e.clientY - boundingRect.top;
+			}
+
+			return {
+				x : mouseX,
+				y : mouseY
+			};
+
+		}

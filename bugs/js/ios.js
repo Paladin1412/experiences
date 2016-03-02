b2120b67b2c3fa4090b5e6d996138d13cf4be109
kocokolo <pengcui@live.com>
2016-03-02 10:12:41 +0800

    Create ios.js

---
+++ bugs/js/ios.js
@@ -0,0 +1,12 @@
+// 解决ios页面滑动问题
+$(window).on("touchmove", function (event_) {
+    var tag = $(event_.target).parents()[0].tagName;
+    var thistag = event_.target.tagName;
+    if (event_.cancelable && tag != "A" && tag != "INPUT" && tag != "TEXTAREA" && tag != "SELECT" && thistag != "A" && thistag != "INPUT" && thistag != "TEXTAREA" && thistag != "SELECT") {
+        event_.preventDefault();
+    }
+});
+// 解决ios双击页面上跳问题
+$(window).bind("touchstart", function (event_) {
+    if (event_.cancelable) event_.preventDefault();
+});
